5c77d6d38abb024d3e2fa54ca68f290c8826f019
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-09 05:02:37 +0600

    21. health component

--- scenes/unit/scripts/unit.gd
+++ scenes/unit/scripts/unit.gd
@@ -6,3 +6,14 @@ class_name Unit
 @onready var visuals: Node2D = %Visuals
 @onready var sprite: Sprite2D = %Sprite
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var health_component: HealthComponent = $HealthComponent
+
+func _ready() -> void:
+	health_component.setup(stats)
+
+func _on_hurt_box_component_on_damaged(hitbox: HitBoxComponent) -> void:
+	if health_component.current_health <= 0:
+		return
+	
+	health_component.take_damage(hitbox.damage)
+	print("%s: %d" % [name, health_component.current_health])
