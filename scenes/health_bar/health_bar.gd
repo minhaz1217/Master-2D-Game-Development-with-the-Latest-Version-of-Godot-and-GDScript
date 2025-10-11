c69e8006feb85b262cf599714ed66b486ee31657
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-11 17:32:47 +0600

    22. health bar

---
+++ scenes/health_bar/health_bar.gd
@@ -0,0 +1,16 @@
+extends Control
+class_name HealthBar
+
+@export var back_color: Color
+@export var fill_color: Color
+
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var label: Label = $Label
+
+func _ready() -> void:
+	var back_style := progress_bar.get_theme_stylebox("background").duplicate()
+	back_style.bg_color = back_color
+	
+	
+	var fill_style := progress_bar.get_theme_stylebox("fill").duplicate()
+	fill_style.bg_color = fill_color
