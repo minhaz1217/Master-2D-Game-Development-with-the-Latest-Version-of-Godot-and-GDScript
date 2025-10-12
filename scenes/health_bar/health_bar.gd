a0cbb4e9ff30008da24eed96c180d1c9c645bb6a
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-12 23:21:53 +0600

    overriding progressbar style

--- scenes/health_bar/health_bar.gd
+++ scenes/health_bar/health_bar.gd
@@ -10,7 +10,11 @@ class_name HealthBar
 func _ready() -> void:
 	var back_style := progress_bar.get_theme_stylebox("background").duplicate()
 	back_style.bg_color = back_color
-	
-	
+
 	var fill_style := progress_bar.get_theme_stylebox("fill").duplicate()
 	fill_style.bg_color = fill_color
+	
+	progress_bar.add_theme_stylebox_override("background", back_style)
+	progress_bar.add_theme_stylebox_override("fill", fill_style)
+
+#func update_bar(value: float)
