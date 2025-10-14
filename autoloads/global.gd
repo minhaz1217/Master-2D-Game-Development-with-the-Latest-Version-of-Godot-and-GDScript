b0b7015767029e8e159f05e0af4bac683dc91f0d
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-14 23:16:34 +0600

    23. add flash

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,3 +1,5 @@
 extends Node
 
+const FLASH_MATERIAL = preload("uid://coi4nu8ohpgeo")
+
 var player: Player
--- scenes/unit/scripts/unit.gd
+++ scenes/unit/scripts/unit.gd
@@ -7,13 +7,24 @@ class_name Unit
 @onready var sprite: Sprite2D = %Sprite
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var flash_timer: Timer = $FlashTimer
 
 func _ready() -> void:
 	health_component.setup(stats)
+
+func set_flash_material() -> void:
+	sprite.material = Global.FLASH_MATERIAL
+	flash_timer.start()
 
 func _on_hurt_box_component_on_damaged(hitbox: HitBoxComponent) -> void:
 	if health_component.current_health <= 0:
 		return
 	
+	set_flash_material()
+	
 	health_component.take_damage(hitbox.damage)
 	print("%s: %d" % [name, health_component.current_health])
+
+
+func _on_flash_timer_timeout() -> void:
+	sprite.material = null
