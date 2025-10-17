eebf5c35fd793bb7bf5894533e233ac9da2cec5c
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-17 23:51:25 +0600

    28. configure weapons

--- resources/items/weapons/weapon_stats.gd
+++ resources/items/weapons/weapon_stats.gd
@@ -3,10 +3,11 @@ class_name WeaponStats
 
 @export var damage := 1.0
 @export_range(0.0, 1.0) var accuracy := 0.9
-@export_range(0.5, 3.0) var cooldown := 1
+@export_range(0.1, 3.0) var cooldown := 1.0
 @export_range(0.0, 1.0) var crit_chance := 0.05
 
 @export var crit_damage := 1.5
+@export var max_range := 150.0
 @export var knockback := 0.0
 @export_range(0.0, 1.0) var lifesteal := 0.0
 
