ab448a9ec39570fba136463cfb0fcf17b543989a
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-10-17 23:41:21 +0600

    28. adding classes for weapons

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -6,7 +6,16 @@ signal on_create_damage_text(unit: Node2D, hitbox: HitBoxComponent)
 const FLASH_MATERIAL = preload("uid://coi4nu8ohpgeo")
 const FLOATING_TEXT = preload("uid://c1sti5gh5yk0y")
 
+enum UpgradeTier{
+	COMMON,
+	RARE,
+	EPIC,
+	LEGENDARY	
+}
+
 var player: Player
+
+
 
 func get_change_success(chance: float) -> bool:
 	var random := randf_range(0, 1.0)
